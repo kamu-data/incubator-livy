74a2f610c475e637bac98041341761cd9c7035e9
Sergii Mikhtoniuk <smikhtoniuk@demonware.net>
2019-08-17 18:13:22 -0700

    kamu: Register types after thriftserver creates a sub-session

--- thriftserver/session/src/main/java/org/apache/livy/thriftserver/session/ThriftSessionState.java
+++ thriftserver/session/src/main/java/org/apache/livy/thriftserver/session/ThriftSessionState.java
@@ -28,6 +28,8 @@ import org.apache.spark.sql.types.StructType;
 
 import org.apache.livy.JobContext;
 
+import org.datasyslab.geosparksql.utils.GeoSparkSQLRegistrator;
+
 /**
  * State related to one Thrift session. One instance of this class is stored in the session's
  * shared object map for each Thrift session that connects to the backing Livy session.
@@ -77,6 +79,8 @@ class ThriftSessionState {
     this.sessionId = sessionId;
     this.statements = new ConcurrentHashMap<>();
     this.spark = ctx.<SparkSession>sparkSession().newSession();
+
+    GeoSparkSQLRegistrator.registerAll(this.spark);
   }
 
   SparkSession spark() {
